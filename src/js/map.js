7b6f700a3f6a1f0d65e7f532bcfb7d783e1d27f0
Trapper1622 <titoutitiwow@gmail.com>
2018-03-02 20:56:30 +0100

    jj

--- src/js/map.js
+++ src/js/map.js
@@ -1,1 +1,13 @@
 /* jshint esversion: 6 */
+
+// --------------- inistialisation google map  ---------------
+// -----------------------------------------------------------
+function initMap() {
+  map = new google.maps.Map(document.querySelector("#map"), {
+    center: {
+      lat: 45.7616,
+      lng: 4.8559
+    },
+    zoom: 13
+  });
+}
