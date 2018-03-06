9c0cb1b4a1b9812e45551537977a965b971c4d31
Trapper <titoutitiwow@gmail.com>
2018-03-06 14:23:51 +0100

    ff

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -1,1 +1,23 @@
 /* jshint esversion: 6 */
+
+//Exécute un appel ajaxGET
+//Prend en paramètre l'URL cible et la fonction callback appelée en cas de succès
+function ajaxGET(url, callback) {
+  var req = new XMLHttpRequest();
+  req.open("GET", url);
+  req.addEventListener("load", function () {
+    if (req.status >= 200 && req.status < 400) {
+      //Appelle la fonction callback en lui passant la réponse de la requète
+      callback(req.responseText);
+    }
+    else {
+      console.error(req.status + " " + req.statusText + " " + url);
+      // console.error(`${req.status} ${req.statusText} ${url}`);
+    }
+  });
+  req.addEventListener("error", function () {
+    console.error("Erreur réseau avec l'URL " + url);
+    // console.error(`Erreur réseau avec l'URL ${url}`);
+  });
+  req.send(null);
+}
--- src/js/map.js
+++ src/js/map.js
@@ -1,5 +1,65 @@
 /* jshint esversion: 6 */
 
+ajaxGET(
+  "https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=1ee3867fba30e0756919f69563ad164308c25536",
+  (reponse) => {
+    const api = JSON.parse(reponse);
+    let markers = [];
+    let stationClick;
+    for (let i = 0; i < api.length; i++) {
+      const iconMarker = {
+        url: "js/images/markerGreen.png",
+        origins: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 20)
+      };
+
+      if (api[i].available_bikes === 0) {
+        iconMarker.url = "js/images/markerOrange.png";
+      }
+      else if (api[i].status === "CLOSE") {
+        iconMarker.url = "js/images/markerRed.png";
+      }
+
+      const marker = new google.maps.Marker( {
+        position: api[i].position,
+        map: map,
+        title: api[i].address,
+        icon: iconMarker
+      });
+
+      // --------------- listener sur les markers ---------------
+      //---------------------------------------------------------
+
+      google.maps.event.addListener(marker, "click", () => {
+        stationClick = api[i];
+        const reservation = document.querySelector("#reservation");
+        const reserver = document.querySelector("#reserver");
+        const confirmation = document.querySelector("#confirmation");
+
+        reservation.style.display = "block";
+        reserver.style.display = "block";
+        confirmation.style.display = "none";
+        reservation.scrollIntoView();
+
+        if (api[i].status === "OPEN") {
+          api[i].status = "OUVERTE";
+        }
+        else if (api[i].status === "CLOSE") {
+          api[i].status = "FERMÉE";
+        }
+
+        info.innerHTML = `
+          <p>Adresse : <span>${api[i].address}</span></p>
+          <p>Etat de la station: <span>${api[i].status}</span></p>
+          <p>Vélo disponibles: <span>${api[i].available_bikes}</span></p>
+          <p>Places disponibles: <span>${api[i].available_bikes_stands}</span></p>`;
+      });
+      markers = markers.concat(marker);
+    }
+  }
+);
+
+
 // --------------- inistialisation google map  ---------------
 // -----------------------------------------------------------
 function initMap() {
